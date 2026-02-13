cd14723a4ab9c72536dbb7ca18257e3aa133112f
HMPorl <paul.scott@thehireman.co.uk>
2026-02-13 17:14:24 +0000

    Add test page to debug sidebar issue

---
+++ pages/0_Test.py
@@ -0,0 +1,8 @@
+# Simple test page - no imports
+import streamlit as st
+
+st.set_page_config(page_title="Test Page", page_icon="🧪")
+
+st.title("🧪 Test Page")
+st.write("If you can see this, pages are working!")
+st.write("The issue is likely with imports in other pages.")
